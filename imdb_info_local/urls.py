9ed7d6f9e7e4ee4efee8835bc91f6aec2f8ff45e
Dave Rogers <dave@drogers.us>
2021-08-05 23:46:17 -0400

    add update functionality
    * update ratings and blurb from javascript via
      custom post call

--- imdb_info_local/urls.py
+++ imdb_info_local/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('movies/mtime/', views.MtimeTitleListView.as_view(), name='movie_mtime'),
     path('tv/', views.TitleListView.as_view(title_type='TV'), name='tv_list'),
     path('tv/mtime/', views.MtimeTitleListView.as_view(title_type='TV'), name='tv_mtime'),
+    path('update/', views.update_title_data, name='title_update')
 ]
--- imdb_info_local/views.py
+++ imdb_info_local/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.views import generic
+from django.http import JsonResponse
 
 from .models import IMDBTitleSearchData
+from .imdb import imdb_title_data
 
 
 class TitleListView(generic.ListView):
@@ -27,3 +31,26 @@ class MtimeTitleListView(TitleListView):
             return IMDBTitleSearchData.objects.filter(type=IMDBTitleSearchData.MOVIE).order_by('-file_mtime')
         elif self.title_type == 'TV':
             return IMDBTitleSearchData.objects.filter(type=IMDBTitleSearchData.TV).order_by('-file_mtime')
+
+
+def update_title_data(request):
+    post_data = json.load(request)['post_data']
+    title = post_data['title']
+    type = post_data['video_type']
+    title_url = post_data['url']
+    result = IMDBTitleSearchData.objects.filter(title=title, type=type)
+    if result and len(result) == 1:
+        target = result[0]
+        new_title_data = imdb_title_data(title_url)
+        target.rating = new_title_data.rating
+        target.blurb = new_title_data.blurb
+        target.save()
+        return_data = {
+            'rating': new_title_data.rating,
+            'blurb': new_title_data.blurb
+        }
+    else:
+        return_data = {
+            'error': 'Either no match in db or more than 1 title for video type (no bueno).'
+        }
+    return JsonResponse(return_data)
